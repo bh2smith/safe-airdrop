24a488f90e03cab2e60b2eecc3cc18ba2597db60
Ben Smith <bh2smith@gmail.com>
2021-05-06 13:10:53 +0200

    add new files

---
+++ src/__tests__/buildTransfers.test.ts
@@ -0,0 +1,188 @@
+import { expect } from "chai";
+import { buildTransfers, TEN } from "../transfers";
+import { SafeInfo, Transaction } from "@gnosis.pm/safe-apps-sdk";
+import { TokenInfo } from "@uniswap/token-lists";
+import BigNumber from "bignumber.js";
+import { Payment } from "../components/CSVForm";
+import { fetchTokenList, TokenMap } from "src/tokenList";
+import { utils, Bytes } from "ethers";
+
+let dummySafeInfo: SafeInfo = {
+  safeAddress: "0x123",
+  network: "rinkeby",
+  ethBalance: "100",
+};
+
+let tokenList: TokenMap;
+let listedTokens: string[];
+let listedToken: TokenInfo;
+const receiverAddress = "0x1000000000000000000000000000000000000000";
+const unlistedToken: TokenInfo = {
+  address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  decimals: 18,
+  symbol: "UNL",
+  name: "Unlisted",
+  chainId: -1,
+};
+
+// TODO - make method erc20TransferData and replace data checks with this instead of hardcoded strings.
+// function erc20TransferData(amount: BigNumber, receiver: string): Bytes {}
+
+describe("Build Transfers:", () => {
+  beforeEach(async () => {
+    tokenList = await fetchTokenList(dummySafeInfo.network);
+    listedTokens = Array.from(tokenList.keys());
+    listedToken = tokenList.get(listedTokens[0]);
+  });
+
+  function listedUnlistedAndNativePayments(
+    amount,
+    receiver
+  ): [Payment, Payment, Payment] {
+    return [
+      // Listed ERC20
+      {
+        receiver,
+        amount: amount,
+        tokenAddress: listedToken.address,
+        decimals: listedToken.decimals,
+      },
+      // Unlisted ERC20
+      {
+        receiver,
+        amount: amount,
+        tokenAddress: unlistedToken.address,
+        decimals: unlistedToken.decimals,
+      },
+      // Native Asset
+      {
+        receiver,
+        amount: amount,
+        tokenAddress: null,
+        decimals: null,
+      },
+    ];
+  }
+
+  describe("Integers", () => {
+    it("works with large integers on listed, unlisted and native asset transfers", () => {
+      let max_amount = new BigNumber(2 ** 255 - 1).dividedBy(
+        TEN.pow(listedToken.decimals)
+      );
+      let fractional_amount = new BigNumber("0.0000001");
+      let large_payments = listedUnlistedAndNativePayments(
+        max_amount,
+        receiverAddress
+      );
+
+      let [listedTransfer, unlistedTransfer, nativeTransfer] = buildTransfers(
+        dummySafeInfo,
+        large_payments,
+        tokenList
+      );
+      expect(listedTransfer.value).to.be.equal("0");
+      expect(listedTransfer.to).to.be.equal(listedToken.address);
+      expect(listedTransfer.data).to.be.equal(
+        "0xa9059cbb0000000000000000000000001000000000000000000000000000000000000000800000000000016c889a28c160ce0422bb9138ff1d4e48274000000000000000"
+      );
+
+      expect(unlistedTransfer.value).to.be.equal("0");
+      expect(unlistedTransfer.to).to.be.equal(unlistedToken.address);
+      expect(unlistedTransfer.data).to.be.equal(
+        "0xa9059cbb0000000000000000000000001000000000000000000000000000000000000000800000000000016c889a28c160ce0422bb9138ff1d4e48274000000000000000"
+      );
+
+      expect(nativeTransfer.value).to.be.equal(
+        max_amount.multipliedBy(TEN.pow(18)).toFixed()
+      );
+      expect(nativeTransfer.to).to.be.equal(receiverAddress);
+      expect(nativeTransfer.data).to.be.equal("0x");
+    });
+  });
+
+  describe("Decimals", () => {
+    it("works with decimal payments on listed, unlisted and native transfers", () => {
+      let fractional_amount = new BigNumber("0.0000001");
+      let small_payments = listedUnlistedAndNativePayments(
+        fractional_amount,
+        receiverAddress
+      );
+      let [listed, unlisted, native] = buildTransfers(
+        dummySafeInfo,
+        small_payments,
+        tokenList
+      );
+      expect(listed.value).to.be.equal("0");
+      expect(listed.to).to.be.equal(listedToken.address);
+      expect(listed.data).to.be.equal(
+        "0xa9059cbb0000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000174876e800"
+      );
+
+      expect(unlisted.value).to.be.equal("0");
+      expect(unlisted.to).to.be.equal(unlistedToken.address);
+      expect(unlisted.data).to.be.equal(
+        "0xa9059cbb0000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000174876e800"
+      );
+
+      expect(native.value).to.be.equal(
+        fractional_amount.multipliedBy(TEN.pow(18)).toFixed()
+      );
+      expect(native.to).to.be.equal(receiverAddress);
+      expect(native.data).to.be.equal("0x");
+    });
+  });
+
+  describe("Mixed", () => {
+    it("works with arbitrary amount strings on listed, unlisted and native transfers", () => {
+      let amount = new BigNumber("123456.000000789");
+      let payments = listedUnlistedAndNativePayments(amount, receiverAddress);
+      let [listed, unlisted, native] = buildTransfers(
+        dummySafeInfo,
+        payments,
+        tokenList
+      );
+      expect(listed.value).to.be.equal("0");
+      expect(listed.to).to.be.equal(listedToken.address);
+      expect(listed.data).to.be.equal(
+        "0xa9059cbb0000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000001a24902beecbd5109200"
+      );
+
+      expect(unlisted.value).to.be.equal("0");
+      expect(unlisted.to).to.be.equal(unlistedToken.address);
+      expect(unlisted.data).to.be.equal(
+        "0xa9059cbb0000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000001a24902beecbd5109200"
+      );
+
+      expect(native.value).to.be.equal(
+        amount.multipliedBy(TEN.pow(18)).toFixed()
+      );
+      expect(native.to).to.be.equal(receiverAddress);
+      expect(native.data).to.be.equal("0x");
+    });
+  });
+
+  describe("Truncation on too many decimals", () => {
+    it("works with arbitrary amount strings on listed, unlisted and native transfers", () => {
+      let amount = new BigNumber("1.000000789");
+      const crappyToken: TokenInfo = {
+        address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+        decimals: 0,
+        symbol: "NOD",
+        name: "No Decimals",
+        chainId: -1,
+      };
+      let payment: Payment = {
+        receiver: receiverAddress,
+        amount: amount,
+        tokenAddress: crappyToken.address,
+        decimals: crappyToken.decimals,
+      };
+      let [transfer] = buildTransfers(dummySafeInfo, [payment], tokenList);
+      expect(transfer.value).to.be.equal("0");
+      expect(transfer.to).to.be.equal(crappyToken.address);
+      expect(transfer.data).to.be.equal(
+        "0xa9059cbb00000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001"
+      );
+    });
+  });
+});
---
+++ src/transfers.ts
@@ -0,0 +1,52 @@
+import IERC20 from "@openzeppelin/contracts/build/contracts/IERC20.json";
+import { AbiItem } from "web3-utils";
+import { SafeInfo, Transaction } from "@gnosis.pm/safe-apps-sdk";
+import { initWeb3 } from "./connect";
+import { TokenMap } from "./tokenList";
+import BigNumber from "bignumber.js";
+import { Payment } from "./components/CSVForm";
+
+export const TEN = new BigNumber(10);
+
+export function buildTransfers(
+  safeInfo: SafeInfo,
+  transferData: Payment[],
+  tokenList: TokenMap
+): Transaction[] {
+  const web3 = initWeb3(safeInfo.network);
+  const erc20 = new web3.eth.Contract(IERC20.abi as AbiItem[]);
+  const txList: Transaction[] = transferData.map((transfer, index) => {
+    if (transfer.tokenAddress === null) {
+      return {
+        to: transfer.receiver,
+        value: transfer.amount.multipliedBy(TEN.pow(18)).toFixed(),
+        data: "0x",
+      };
+    } else {
+      const exponent = new BigNumber(
+        TEN.pow(
+          tokenList.get(transfer.tokenAddress)?.decimals || transfer.decimals
+        )
+      );
+      let amountData = transfer.amount.multipliedBy(exponent);
+      if (amountData.decimalPlaces() > 0) {
+        // TODO - reinstate this warning by passing along with return content
+        // Return (Transaction[], Message)
+        // setLastError({
+        //   message:
+        //     "Precision too high. Some digits are ignored for row " + index,
+        // });
+        amountData = amountData.decimalPlaces(0, BigNumber.ROUND_DOWN);
+      }
+
+      return {
+        to: transfer.tokenAddress,
+        value: "0",
+        data: erc20.methods
+          .transfer(transfer.receiver, amountData.toFixed())
+          .encodeABI(),
+      };
+    }
+  });
+  return txList;
+}
