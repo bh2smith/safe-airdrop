aca43f2fad2e102f617ebaa395d675cb4d5f098b
Ben Smith <bh2smith@gmail.com>
2021-05-15 14:16:51 +0200

    comment react-app to stop complaining about weird code break

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "plugin:import/recommended",
     "plugin:import/typescript",
     "plugin:prettier/recommended",
-    "react-app",
+    // "react-app",
   ],
   ignorePatterns: ["build/", "node_modules/", "!.prettierrc.js", "lib/"],
   rules: {
