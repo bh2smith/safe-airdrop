63e18897898963f7feb0538815a9dcc6b986e2fe
Ben Smith <bh2smith@gmail.com>
2022-02-05 19:32:12 +0100

    update all safe dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
   plugins: ["import"],
-  extends: [
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-    "react-app",
-    "prettier",
-  ],
+  extends: ["plugin:import/typescript", "plugin:prettier/recommended", "react-app", "prettier"],
   ignorePatterns: ["build/", "node_modules/", "!.prettierrc.js", "lib/"],
   rules: {
     "import/order": [
